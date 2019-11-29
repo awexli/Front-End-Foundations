14c8525b8db4167eed0bf2c0fab39df1a749031d
awexli <ali150@mail.ccsf.edu>
2019-11-28 21:52:53 -0800

    Add ability to fetch dog breeds from dropdown menu
    [+] Add init() that populates dropdown menu with breed options
    [+] Add event listener for dropdown menu
    [+] Add addDog() the fetches selected dog breed

--- Doggos/doggos.js
+++ Doggos/doggos.js
@@ -1,21 +1,42 @@
-const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
-
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+const INIT_URL = 'https://dog.ceo/api/breeds/image/random';
 const doggos = document.querySelector('.doggos');
+const select = document.querySelector('.breeds');
+const img = document.querySelector('.doggo-img');
 
-function addDoggo() {
+function init(BREEDS_URL) {
     fetch(BREEDS_URL)
         .then(response => response.json())
         .then(data => {
-            const img = document.createElement('img');
+            const breedsObject = data.message;
+            const breedsArray = Object.keys(breedsObject);
+
+            for (let i = 0; i < breedsArray.length; i++) {
+                const option = document.createElement('option');
+                option.value = breedsArray[i];
+                option.innerText = breedsArray[i];
+                select.appendChild(option);
+            }
+        });
+        
+    addDog(INIT_URL);
+}
+
+function addDog(doggo) {
+    //show loading spinner
+    fetch(doggo)
+        .then(response => response.json())
+        .then(data => {
             img.src = data.message;
             img.alt = 'Cute Doggo';
             doggos.appendChild(img);
+            //stop loading spinner
         });
-
 }
 
-document.querySelector('.add-doggo').addEventListener
-    ('click', addDoggo)
+select.addEventListener('change', event => {
+    let doggo = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+    addDog(doggo);
+});
 
-
-console.log("this will log first");
+init(BREEDS_URL);
