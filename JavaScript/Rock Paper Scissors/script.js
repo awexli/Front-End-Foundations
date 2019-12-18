a30e8aa8cea980c92f52a45fdee382728226f6f7
awexli <ali150@mail.ccsf.edu>
2019-12-17 18:27:33 -0800

    refactor code, add win condition

--- JavaScript/Rock Paper Scissors/script.js
+++ JavaScript/Rock Paper Scissors/script.js
@@ -1,64 +1,3 @@
-function computerPlay() {
-  // 0 = rock
-  // 1 = paper
-  // 2 = scissors
-  const min = Math.ceil(0);
-  const max = Math.floor(2);
-  
-  // max and min included
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function playRound(playerSelection, computerSelection) {
-  let message;
-  // Rock
-  if (playerSelection === 0) {
-    if (computerSelection === 0) {
-      message = "Both players chose Rock";
-    }
-    
-    if (computerSelection === 1) {
-      message = "You Lose, Paper beats Rock";
-    }
-
-    if (computerSelection === 2) {
-      message = "You Win, Rock beats Scissors"
-    }
-  }
-
-  // Paper
-  if (playerSelection === 1) {
-    if (computerSelection === 0) {
-      message = "You Win, Paper beats Rock";
-    }
-    
-    if (computerSelection === 1) {
-      message = "Both players chose Paper";
-    }
-
-    if (computerSelection === 2) {
-      message = "You Lose, Scissors beats Paper"
-    }
-  }
-
-  // Scissors
-  if (playerSelection === 2) {
-    if (computerSelection === 0) {
-      message = "You Lose, Rock beats Scissors";
-    }
-    
-    if (computerSelection === 1) {
-      message = "You Win, Scissors beats Paper";
-    }
-
-    if (computerSelection === 2) {
-      message = "Both players chose Scissors"
-    }
-  }
-
-  document.getElementById("result").innerHTML = message;
-}
-
 function game() {
   const button = document.querySelectorAll('.button');
   const rock = document.querySelector('#rock');
@@ -66,19 +5,15 @@ function game() {
   const scissor = document.querySelector('#scissor');
 
   let round = 0;
-  
+  let playerScore = 0;
+  let compScore = 0;
+
   button.forEach(choice => {
     choice.addEventListener('click', () => {
         if (round < 5) {
-          document.getElementById("round").innerHTML = "Round " + (round + 1);
+          document.getElementById("round").innerHTML = (round + 1);
           round++;
         } 
-
-        if (round == 5) {
-          rock.disabled = true;
-          paper.disabled = true;
-          scissor.disabled = true;
-        }
     });
   });
   
@@ -94,6 +29,99 @@ function game() {
     playRound(2, computerPlay());
   })
 
+  function computerPlay() {
+    // 0 = rock
+    // 1 = paper
+    // 2 = scissors
+    const min = Math.ceil(0);
+    const max = Math.floor(2);
+    
+    // max and min included
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function playRound(playerSelection, computerSelection) {
+    let message;
+    let playerWin = false;
+    // Rock
+    if (playerSelection === 0) {
+      if (computerSelection === 0) {
+        message = "Both players chose Rock";
+      }
+      
+      if (computerSelection === 1) {
+        message = "You Lose, Paper beats Rock";
+      }
+  
+      if (computerSelection === 2) {
+        message = "You Win, Rock beats Scissors"
+        playerWin = true;
+      }
+    }
+  
+    // Paper
+    if (playerSelection === 1) {
+      if (computerSelection === 0) {
+        message = "You Win, Paper beats Rock";
+        playerWin = true;
+      }
+      
+      if (computerSelection === 1) {
+        message = "Both players chose Paper";
+      }
+  
+      if (computerSelection === 2) {
+        message = "You Lose, Scissors beats Paper"
+      }
+    }
+  
+    // Scissors
+    if (playerSelection === 2) {
+      if (computerSelection === 0) {
+        message = "You Lose, Rock beats Scissors";
+      }
+      
+      if (computerSelection === 1) {
+        message = "You Win, Scissors beats Paper";
+        playerWin = true;
+      }
+  
+      if (computerSelection === 2) {
+        message = "Both players chose Scissors"
+      }
+    }
+  
+    document.getElementById("result").innerHTML = message;
+  
+    updateScore(playerWin);
+  }
+
+  function updateScore(result) {
+    if (result) {
+      playerScore++;
+    } else {
+      compScore++;
+    }
+
+    if (playerScore > 2) {
+      document.getElementById("winner").innerHTML = "You Win!"
+      disableButtons();
+    } 
+
+    if (compScore > 2) {
+      document.getElementById("winner").innerHTML = "You Lose!"
+      disableButtons();
+    }
+  
+    document.getElementById("player").innerHTML = playerScore;
+    document.getElementById("comp").innerHTML = compScore;
+  }
+
+  function disableButtons() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissor.disabled = true;
+  }
 }
 
 game();
