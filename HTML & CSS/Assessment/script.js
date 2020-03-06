e1a4b96b0e61138c37f8734cc817fd78d36510cc
awexli <awexli@mail.sfsu.edu>
2020-03-05 20:08:45 -0800

    complete assignment

---
+++ HTML & CSS/Assessment/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", e => {
+    if (e.target.className == "box") {
+        alert(e.target.firstElementChild.innerText);
+    }
+    if (e.target.className == "name") {
+        alert(e.target.innerText);
+    }
+})
