0902868b8dc049da0acd49fdc3987279c3abffb8
awexli <awexli@mail.sfsu.edu>
2020-03-06 18:16:44 -0800

    add async_exc.js
    - 6/7 challenges completed

--- HTML & CSS/Assessment/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", e => {
-    if (e.target.className == "box") {
-        alert(e.target.firstElementChild.innerText);
-    }
-    if (e.target.className == "name") {
-        alert(e.target.innerText);
-    }
-})
